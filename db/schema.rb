95b0a91416ca74c54cbb8908cb80116b6f7c96a6
Marco Besier <marcobesier@icloud.com>
2020-08-26 13:18:48 +0200

    Add created_at and updated_at attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_110353) do
+ActiveRecord::Schema.define(version: 2020_08_26_111558) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
