ef7dd85242b212a06a31444525dbd1ea56ac1f8f
Marco Besier <marcobesier@icloud.com>
2020-11-13 09:58:47 +0100

    Fix punctuation in alert messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
 
   def require_same_user
     if current_user != @user && !current_user.admin?
-      flash[:alert] = "You can only edit or delete your own account"
+      flash[:alert] = "You can only edit or delete your own account."
       redirect_to @user
     end
   end
